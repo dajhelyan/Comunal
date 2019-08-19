75afdab197f065800bef650b5dcfd84c56b12fd6
dajhelyan <assiaxramx10@gmail.com>
2019-08-19 16:09:25 -0500

    Agragando configuracion de firebase a main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,18 @@
 import {init} from './view-controller/routes.js';
 
 export const initFirebase = () => {
-    const 
+    const firebaseConfig = {
+        apiKey: "AIzaSyDaAmhGu9QUW3cmaoaQWm5Speouts2Qvr0",
+        authDomain: "comunal-3f620.firebaseapp.com",
+        databaseURL: "https://comunal-3f620.firebaseio.com",
+        projectId: "comunal-3f620",
+        storageBucket: "",
+        messagingSenderId: "691116814616",
+        appId: "1:691116814616:web:f439fe74fbddd90b"
+    }
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    initRouter();
 }
+
+window.onload = initFirebase();

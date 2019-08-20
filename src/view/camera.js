2a2c0def6cff80eb2b8e974bb6e1f270e95ec670
SusanaCahuantico <scahuantico@gmail.com>
2019-08-20 00:37:54 -0500

    creando funcion capturar foto desde la webcam

---
+++ src/view/camera.js
@@ -0,0 +1,28 @@
+export const Camera = () => {
+    const createDiv = document.createElement('div');
+    const camera = `
+    <video id="video" autoplay> Video </video>
+    <button id="foto">foto </button>
+        <canvas id="canvas" width="200" height="200"></canvas>
+    `;
+    createDiv.innerHTML = camera;
+
+    const video = createDiv.querySelector('#video')
+    const foto = createDiv.querySelector('#foto')
+    const canvas = createDiv.querySelector('#canvas')
+    const start = () => {
+        navigator.mediaDevices.getUserMedia({
+            audio: false,
+            video: true
+        }).then(stream => {
+            video.srcObject = stream;
+        }).catch(console.error)
+    }
+    window.addEventListener('load', start, false);
+    var context = canvas.getContext('2d');
+    foto.addEventListener('click', () => {
+        context.drawImage(video, 0,0, 200,200);
+    })
+
+    return createDiv;
+}
--- src/view/register.js
+++ src/view/register.js
@@ -1,7 +1,7 @@
-import { changeHash } from "../view-controller/firestore.js";
-
+//import { changeHash } from "../view-controller/firestore.js";
 
 export const registerVisitor = () => {
+    const sectionRegister = document.createElement('section');
     const tmplRegisterVisitor = `
     <div>
         <h1>Comunal</h1>
@@ -19,24 +19,12 @@ export const registerVisitor = () => {
             <option value="">Amigos y familia</option>
         </select>
         <p>Propósito de visita</p>
-        <button>foto</button>
+        <a href="#/camera"> Cam </a>
         <button type="button" id="register-visitor">Registrar visita</button>
     </div>
     `;
 
-    const sectionRegister = document.createElement('section');
     sectionRegister.innerHTML = tmplRegisterVisitor;
-    const foto = document.querySelector('#foto')
-    window.URL = window.URL || window.webkitURL;
-    navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
-
-    navigator.getUserMedia({video:true}, function(vid){
-        document.querySelector('video').src = window.URL.createObjectURL(vid);
-    });
-
-    sectionRegister.querySelector('#register-visitor').addEventListener('click', () => {
-        return changeHash('#/welcome')
-    })
 
     return sectionRegister;
 }

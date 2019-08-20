82591301c43943f49168f2887cfc81141938bf3e
ElviaVega <elvita100@hotmail.com>
2019-08-20 10:01:02 -0500

    enviando datos a firebase

--- src/view/register.js
+++ src/view/register.js
@@ -1,17 +1,19 @@
 import { changeHash } from "../view-controller/firestore.js";
-
+import { sendToUsers } from "../controller/functions.js";
 
 export const registerVisitor = () => {
     const tmplRegisterVisitor = `
     <div>
         <h1>Comunal</h1>
-        <input type="text"> <span><button>+</button><button>-</button></span>
-        <p>Nombre completo</p> 
-        <input type="text">
+        <input type="text" id="dni" placeholder ='DNI o Carnet de Extranjeria'>
         <p>DNI o Carnet de Extranjeria</p>
-        <input type="text">
+        <input type="text" id="nombre" placeholder ='Nombre Completo' > <span><button>+</button><button>-</button></span>
+        <p>Nombre completo</p> 
+        <input type='email' id='email' placeholder ='Email'>
+        <p>Correo electrónico</p>
+        <input type="text" id="empresa" placeholder ='Empresa'>
         <p>Empresa</p>
-        <input type="text">
+        <input type="text" id="host" placeholder ='Anfitrión(a)'>
         <p>Anfitrión(a)</p>
         <select name="" id="purposeOfVisit" placeHolder="seleccionar">
             <option value="">reunión</option>
@@ -24,10 +26,33 @@ export const registerVisitor = () => {
     </div>
     `
 
+
+
+
+
+
+
+
+
+
+
+
+
     const sectionRegister = document.createElement('section');
     sectionRegister.innerHTML = tmplRegisterVisitor;
 
+    const dni = sectionRegister.querySelector('#dni');
+    const nombre = sectionRegister.querySelector('#nombre');
+    const email = sectionRegister.querySelector('#email');
+    const empresa = sectionRegister.querySelector('#empresa'); 
+    const host = sectionRegister.querySelector('#host')
+    //const proposito = sectionRegister.querySelector('#purposeOfVisit')
+
+    
+     
     sectionRegister.querySelector('#register-visitor').addEventListener('click', () => {
+        sendToUsers(nombre.value, dni.value, email.value, empresa.value, host.value )
+        
         return changeHash('#/welcome')
     })
 

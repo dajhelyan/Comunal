25ff5cf9b712df660b49d1107d12edd81a56ee7e
SusanaCahuantico <scahuantico@gmail.com>
2019-08-21 10:46:53 -0500

    css

--- src/view/home.js
+++ src/view/home.js
@@ -2,13 +2,23 @@ import { changeHash } from '../view-controller/firestore.js'
 
 export const viewHome = () => {
     const tmplHome = `
-    <h3>Bienvenido a Comunal</h3>
-    <button type="button" id="btn-page-register">Registra tu visita</button>
-    <a href="#/user">¿Haz estado aqui antes?</a></br>
-    <a href="#/administrador">Administrador </a>
-    `
-
+    <div>
+    <img src="./image/comunal.png" class="imagenlogo">
+    <div>
+    <button type="button" id="btn-page-register" class="btn-register col-5">Registra tu visita aquí</button>
+    </div>
+      <div class="col-12">
+      <div class="col-3 antes-hover"> <a href="#/administrador" class="admin">Administrador </a> </div>
+      <div class="col-3 admin-hover"> <a href="#/user" class="antes">¿Haz estado aqui antes?</a> </div>
+      </div>
+    </div>
+    `;
+    
     const sectionHome = document.createElement('section');
+    sectionHome.className = 'fondoimage col-12';
+    //const imgFondo = document.createElement('img')
+    //imgFondo.src = "../image/fondo.jpeg";
+    //imgFondo.appendChild(sectionHome);
     sectionHome.innerHTML = tmplHome;
 
     const btnRegisterPage = sectionHome.querySelector('#btn-page-register')
